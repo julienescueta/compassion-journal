79c47d7b0b53741ee7045b1f11f38f5c99ed485f
Julien Escueta <julien.escueta@gmail.com>
2018-12-06 12:47:29 -0800

    run container once provisioned

--- install-docker.sh
+++ install-docker.sh
@@ -3,3 +3,5 @@ yum update
 yum install -y docker
 service docker start
 usermod -aG docker ec2-user
+docker pull julienescueta/compassionjournal
+docker run -d -t --rm -p=80:80 julienescueta/compassionjournal
