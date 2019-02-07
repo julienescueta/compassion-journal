1108abd49f524f8c328374ed353a73dd08ae3ac6
Julien Escueta <julien.escueta@gmail.com>
2019-02-07 11:20:45 -0800

    docker install through amazon-linux-extras

--- install-docker.sh
+++ install-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-yum update
-yum install -y docker
-service docker start
-usermod -aG docker ec2-user
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -aG docker ec2-user
 docker pull julienescueta/compassionjournal
 docker run -d -t --rm -p=80:80 julienescueta/compassionjournal
